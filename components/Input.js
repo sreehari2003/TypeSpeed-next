7d645f66261a17067eaa806c0544250b47292a54
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-01-21 12:50:49 +0530

    timer added

--- components/Input.js
+++ components/Input.js
@@ -2,7 +2,9 @@ import React, { useState, useContext } from "react";
 import TextField from "@material-ui/core/TextField";
 import Text from "./Text";
 import InfoContext from "../context/ScoreContext";
+import Timer from "./Timer";
 const Input = () => {
+  const [tm, setTm] = useState(16);
   const context = useContext(InfoContext);
   const [dis, setDis] = useState(false);
   const getInput = (e) => {
@@ -16,18 +18,26 @@ const Input = () => {
   const getKey = (e) => {
     context.changeKey(e.key);
   };
+  let tms;
   const focus = () => {
+    tms = setInterval(() => {
+      setTm((val) => val - 1);
+    }, [1000]);
     setTimeout(() => {
       setDis(true);
+      clearInterval(tms);
       setTimeout(() => {
+        setTm(16);
         context.reStart();
         setDis(false);
+        context.setInput("");
       }, [5000]);
-    }, [15000]);
+    }, [16000]);
   };
   return (
     <div>
       <Text />
+      <Timer val={tm} />
       <div className="field">
         <TextField
           disabled={dis}
